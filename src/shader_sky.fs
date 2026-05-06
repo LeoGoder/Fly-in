c2eed85b9f94758e2cc242dc31e1d87e193812aa
Leo Goderneaux <lgoderne@z1r1p1.42lyon.fr>
2026-05-06 16:19:35 +0200

    [FIX] shader updated

--- src/shader_sky.fs
+++ src/shader_sky.fs
@@ -6,11 +6,39 @@ uniform float time;
 out vec4 final_color;
 
 // random function
-
+float	hash(vec2 p) {
+	p = fract(p * vec2(123.34, 456.21));
+	p += dot(p, p + 45.32);
+	return fract(p.x * p.y);
+}
 
 // main
 void	main() {
-	vec2 grid = floor(gl_FragCoord.xy * 0.5);
-	vec3 color = vec3(0.8, 0.02, 0.05);
+	vec3 color = vec3(0.02, 0.02, 0.05);
+	vec2 uv = gl_FragCoord.xy / resolution.xy;
+	// vec3 color2 = vec3(0.02, abs(sin(time)), 1.00);
+	float speed = 35.0;
+
+	// Nebuleuse 1
+	float pattern1 = sin(uv.x * 3.0 + time * 0.2) + sin(uv.y * 4.0 - time * 0.1);
+    pattern1 = pattern1 * 0.25 + 0.5;
+    vec3 nebula1 = vec3(0.0, 0.3, 0.4) * pattern1;
+
+	// Nebuleuse 2
+    float pattern2 = sin(uv.x * 5.0 - time * 0.3) + sin(uv.y * 3.0 + time * 0.2);
+    pattern2 = pattern2 * 0.25 + 0.5;
+    vec3 nebula2 = vec3(0.3, 0.0, 0.5) * pattern2;
+
+    color += nebula1 + nebula2;
+	vec2 offset = vec2(0.0, time * speed);
+	vec2 grid = floor((gl_FragCoord.xy + offset) * 0.5);
+	if (hash(grid)> 0.990) {
+		float sparkle = 1.0;
+		// color.r += 0.1;
+		// color.g = abs(sin(time));
+		// color.b = 1.0;
+		color += vec3(1.0) * sparkle;
+	}
 	final_color = vec4(color, 1.0);
 }
+
